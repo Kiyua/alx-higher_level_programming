dd9be37671c182c575ad8a4b415c03b7b311fb5a
Kiyua <bestycf1@gmail.com>
2022-09-28 21:08:41 -0700

    three to nine

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+def only_diff_elements(set_1, set_2):
+    diff = []
+    for elem in set_1:
+        if elem not in set_2:
+            diff.append(elem)
+    for elem in set_2:
+        if elem not in set_1:
+            diff.append(elem)
+    return diff
